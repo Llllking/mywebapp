104087933d283486c38cf06aa23714fc32705266
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-09-03 16:04:04 +0100

    getting started with my webapp

---
+++ webapp1/src/main/java/com/ismail/springboot/webapp1/hello/SayHelloController.java
@@ -0,0 +1,16 @@
+package com.ismail.springboot.webapp1.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello! What are you learning today.";
+    }
+}
