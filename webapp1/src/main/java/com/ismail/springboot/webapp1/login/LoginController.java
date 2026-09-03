e888fe32454225d1601ac611dcf6bba5be4af794
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-09-03 18:55:01 +0100

    playing with @RequestParam and ModelMap and using logger to log instead of sout for the first time

--- webapp1/src/main/java/com/ismail/springboot/webapp1/login/LoginController.java
+++ webapp1/src/main/java/com/ismail/springboot/webapp1/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.ismail.springboot.webapp1.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("login")
-    public String goToLoginPage(@RequestParam String name, ModelMap model){
+    public String goToLoginPage(@RequestParam String name, @RequestParam int age, @RequestParam String country, ModelMap model){
+        logger.info("the name passed to the controller from the request is : {}", name);
         model.put("name", name);
+        model.put("age", age);
+        model.put("country", country);
         return "login";
     }
 }
--- webapp1/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ webapp1/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,6 @@
         <title>Login Page</title>
     </head>
     <body>
-       <p>Welcome to the login page ${name}</p>
+       <p>Welcome to the login page ${name} you are ${age * 100} years old and you live in ${country}</p>
     </body>
 </html>
