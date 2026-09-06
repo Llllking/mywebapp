ffc22b8cd178586e1903e84d0847efea5d3f73ae
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-09-06 19:22:37 +0100

    add addTodo functionality

--- webapp1/src/main/java/com/ismail/springboot/webapp1/todo/TodoService.java
+++ webapp1/src/main/java/com/ismail/springboot/webapp1/todo/TodoService.java
@@ -11,13 +11,20 @@ public class TodoService {
 
     private static List<Todo> todos = new ArrayList<>();
 
+    private static int todosCount = 0;
+
     static {
-        todos.add(new Todo(1, false, LocalDate.now().plusYears(1), "Learn AWS", "Ismail"));
-        todos.add(new Todo(2, false, LocalDate.now().plusYears(2), "Learn Azure", "Ismail"));
-        todos.add(new Todo(3, false, LocalDate.now().plusYears(3), "Learn FullStack dev", "Ismail"));
+        todos.add(new Todo(++todosCount, false, LocalDate.now().plusYears(1), "Learn AWS", "Ismail"));
+        todos.add(new Todo(++todosCount, false, LocalDate.now().plusYears(2), "Learn Azure", "Ismail"));
+        todos.add(new Todo(++todosCount, false, LocalDate.now().plusYears(3), "Learn FullStack dev", "Ismail"));
     }
 
     public List<Todo> findByUsername(String username) {
         return todos.stream().filter(todo -> todo.getUsername().equalsIgnoreCase("ismail")).toList();
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done){
+        Todo todo = new Todo(++todosCount, done, targetDate, description, username);
+        todos.add(todo);
     }
 }
