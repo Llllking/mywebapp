118a18294b496472e28c0c2e205cde0c5a00e6ab
ismailHACHAMI-ALAOUI <ismailhac2002@gmail.com>
2026-09-08 22:19:38 +0100

    add jpa and H2 dependencies and mapping the todo class to a table using @Entity and adding todos in that table using queries in data sql

--- webapp1/src/main/java/com/ismail/springboot/webapp1/security/SpringSecurityConfiguration.java
+++ webapp1/src/main/java/com/ismail/springboot/webapp1/security/SpringSecurityConfiguration.java
@@ -1,13 +1,19 @@
 package com.ismail.springboot.webapp1.security;
 
+import jakarta.annotation.Nonnull;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -18,7 +24,25 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager(){
-        UserDetails user = User.builder().passwordEncoder(password -> passwordEncoder().encode(password)).username("Ismail").password("dummy").roles("ADMIN", "USER").build();
-        return new InMemoryUserDetailsManager(user);
+        UserDetails userDetails1 = createNewUser("Ismail", "dummy");
+        UserDetails userDetails2 = createNewUser("hachami", "dummy2");
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
     }
+
+    @Nonnull
+    private UserDetails createNewUser(String username, String userPassword) {
+        return User.builder().passwordEncoder(password -> passwordEncoder().encode(password)).username(username).password(userPassword).roles("ADMIN", "USER").build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+        return http.build();
+
+    }
+
+
 }
---
+++ webapp1/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (1001, 'Ismail', 'Get AWS Certified', CURRENT_DATE(), false);
